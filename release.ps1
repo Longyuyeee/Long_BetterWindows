957b40bf9be1140d00972b16544f10b974fb07a6
刘若晨 <lrc168278.jx@chinatelecom.cn>
2026-08-15 00:28:27 +0800

    chore: rebase RC17 for formal validation

--- release.ps1
+++ release.ps1
@@ -8,7 +8,7 @@
   .\release.ps1 -PackageKind SelfContained
 #>
 param(
-    [string] $Version = '1.11.0-rc.16',
+    [string] $Version = '1.11.0-rc.17',
     [ValidateSet('All', 'FrameworkDependent', 'SelfContained')]
     [string] $PackageKind = 'All',
     [switch] $Force
